8ee3928cc0091f2e4e73174e6e6bcd094c9a5f0b
John AZ1 <39185@stpats.vic.edu.au>
2018-01-28 15:09:04 +1100

    Skeleton outline

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Heading extends Component {
+  render() {
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+class Filter extends Component {
+  render() {
+    return(
+      <input type="text"/>
+    );
+  }
+}
+class Item extends Component {
+  render() {
+    return(
+      <div id={this.props.id}>
+        <h2>{this.props.name}</h2>
+        <h2>{this.props.lastName}</h2>
+        <h2>{this.props.age}</h2>
+      </div>
+    );
+  }
+}
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="App">
+        <Heading title="Hello World"/>
+        <Filter/>
       </div>
     );
   }
